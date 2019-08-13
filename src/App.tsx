abdb6f5461b9182b48d65d7e54d96ec313758daa
underwater8 <caimengdi@gmail.com>
2019-08-12 21:33:37 -0700

    Add react-redux

--- src/App.tsx
+++ src/App.tsx
@@ -1,36 +1,54 @@
 import React from 'react';
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import './App.css';
+import { createStore, AnyAction, Dispatch } from 'redux';
+import { createReducer } from 'redux-create-reducer'
+import { Provider, connect } from 'react-redux';
+
+
+export interface AppState {
+  aList:string[];
+  aNumber: number;
+  aString: string
+}
+
+const initState:AppState = {
+  aList: ['hello', 'world'],
+  aNumber: 100,
+  aString: 'I am Iron Man.'
+};
+
+const rootReducer = createReducer<AppState, AnyAction>(initState, {
+  'myReducer': (state:AppState, action:AnyAction): AppState => Object.assign({}, initState) 
+}) ;
+
+const store = createStore<AppState, AnyAction, any, any>(rootReducer, initState);
 
 const App: React.FunctionComponent = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-        <AnotherApp />
-      </header>
-    </div>
+    <Provider store={store} >
+        <AnotherAppContainer />
+    </Provider>
   );
 }
 
-export class AnotherApp extends React.Component {
+export class AnotherApp extends React.Component<AppState,{}> {
   render() {
     return (
-      <h1>Hello world!!! Bye bye world 234 ?</h1>
+      <div>
+        <h1>{this.props.aString}</h1>
+        <h2>{this.props.aNumber}</h2>
+        <ul>
+          {this.props.aList.map((v, k) => (<li key={k}>{v}</li>))}
+        </ul>
+      </div>
     );
   }
 }
 
+const AnotherAppContainer = connect(
+  (state:AppState )=> state, 
+  (dispatch: Dispatch) => ({})
+)(AnotherApp);
 
 export default App;
